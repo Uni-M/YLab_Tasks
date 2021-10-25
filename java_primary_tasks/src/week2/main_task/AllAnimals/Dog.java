5c0d35831538d579e52053ecb83c2bc51e81eea1
Julia Melnikova <61387671+Uni-M@users.noreply.github.com>
2021-10-25 21:30:45 +0300

    Update Dog.java
    Some changes

--- java_primary_tasks/src/week2/main_task/AllAnimals/Dog.java
+++ java_primary_tasks/src/week2/main_task/AllAnimals/Dog.java
@@ -2,6 +2,7 @@ package week2.main_task.AllAnimals;
 
 public class Dog extends Animal{
 
+     String sound = "Bow-bow";
     @Override
     public void eat() {
         setEating(true);
@@ -11,7 +12,7 @@ public class Dog extends Animal{
     @Override
     public void speak() {
         if (!getSleeping()){
-            System.out.println("Bow-bow");
+            System.out.println(sound);
         }
     }
 }
