30a4d4d792553a701f26a3b3ffa41846532d38b7
Julia Melnikova <bukoffka@inbox.ru>
2021-10-26 15:46:56 +0300

    add lesson2 task13 in javarush_ipnodns

--- javarush_ipnodns/src/Lesson2/Task13/Main.java
+++ javarush_ipnodns/src/Lesson2/Task13/Main.java
@@ -9,5 +9,19 @@ package Lesson2.Task13;
 //            МылаРамуМама
 //            РамуМамаМыла
 //            ...
+
 public class Main {
+    public static void main(String[] args) {
+        String a = "Мама";
+        String b = "Мыла";
+        String c = "Раму";
+
+        System.out.println(a+b+c);
+        System.out.println(b+a+c);
+        System.out.println(a+c+b);
+        System.out.println(b+c+a);
+        System.out.println(c+a+b);
+        System.out.println(c+b+a);
+
+    }
 }
