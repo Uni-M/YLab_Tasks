f8439921cce041a4408e04818151bcb3f5e0552d
Julia Melnikova <bukoffka@inbox.ru>
2021-11-01 22:40:13 +0300

    Add Tasks

---
+++ javarush_ipnodns/src/Lesson4/Task12/Main.java
@@ -0,0 +1,13 @@
+package Lesson4.Task12;
+
+// 10 чисел
+//Вывести на экран числа от 1 до 10, используя цикл while.
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10){
+            System.out.println(i++);
+        }
+    }
+}
